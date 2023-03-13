c2b7abc9ba3f2b2c5563c1c8bd9c90550ff40307
Kathy Bauer <kbauer17@my.wctc.edu>
2023-03-13 14:08:06 -0500

    [1.11] Add user menu without new fields

--- Program.cs
+++ Program.cs
@@ -47,4 +47,59 @@ logger.Info(scrubbedFile);
 
 MovieFile movieFile = new MovieFile(scrubbedFile);
 
+string choice = "";
+do
+{
+  // display choices to user
+  Console.WriteLine("1) Add Movie");
+  Console.WriteLine("2) Display All Movies");
+  Console.WriteLine("Enter to quit");
+  // input selection
+  choice = Console.ReadLine();
+  
+  if (choice == "1")
+  {
+    // Add movie
+    Movie movie = new Movie();
+    // ask user to input movie title
+    Console.WriteLine("Enter movie title");
+    // input title
+    movie.title = Console.ReadLine();
+    // verify title is unique
+    if (movieFile.isUniqueTitle(movie.title)){
+      // input genres
+      string input;
+      do
+      {
+        // ask user to enter genre
+        Console.WriteLine("Enter genre (or done to quit)");
+        // input genre
+        input = Console.ReadLine();
+        // if user enters "done"
+        // or does not enter a genre do not add it to list
+        if (input != "done" && input.Length > 0)
+        {
+          movie.genres.Add(input);
+        }
+      } while (input != "done");
+      // specify if no genres are entered
+      if (movie.genres.Count == 0)
+      {
+        movie.genres.Add("(no genres listed)");
+      }
+            // add movie
+      movieFile.AddMovie(movie);
+    }
+  } else if (choice == "2")
+  {
+    // Display All Movies
+    foreach(Movie m in movieFile.Movies)
+    {
+      Console.WriteLine(m.Display());
+    }
+  }
+  logger.Info("User choice: {Choice}", choice);
+} while (choice == "1" || choice == "2");
+
+
 logger.Info("Program ended");
