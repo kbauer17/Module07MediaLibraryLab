2186fd04bcaeba090cc86ed740934a8786211ce1
Kathy Bauer <kbauer17@my.wctc.edu>
2023-03-13 13:41:27 -0500

    [1.02] NLog dotnet add package NLog --version 5.1.1 - Configure nlog targets for output - Create nlog instance

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NLog;
+
+// See https://aka.ms/new-console-template for more information
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+// create instance of Logger
+var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+logger.Info("Program started");
+
+Console.WriteLine("Hello World!");
+
+logger.Info("Program ended");
